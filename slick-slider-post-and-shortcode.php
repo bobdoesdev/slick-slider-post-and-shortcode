f2fc415a25fbce53cba66cfe83fbf2efd8ace84f
Bob <bobobrienpa@gmail.com>
2018-11-21 07:41:05 -0500

    Cleaned up main file

--- slick-slider-post-and-shortcode.php
+++ slick-slider-post-and-shortcode.php
@@ -7,23 +7,6 @@
  * Licence: GPL2
  */
 
-/*  Copyright 2018  Digital Eel Inc.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-// exit if file is called directly
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 require_once plugin_dir_path(__FILE__) . 'classes/class-slick-slides-post-type.php';
